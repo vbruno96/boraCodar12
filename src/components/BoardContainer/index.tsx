4fd7c77b0e76b6ca10d38aa1bfbd49a51f2ac34a
Bruno Vinícius <vbruno96@gmail.com>
2023-03-23 14:55:34 -0300

    creating board container component

---
+++ src/components/BoardContainer/index.tsx
@@ -0,0 +1,30 @@
+import { Card } from '../Card'
+
+interface ICard {
+  title: string
+  content: string
+  tags: [string]
+}
+
+interface BoardContainerProps {
+  title: string
+  cards: ICard[]
+}
+
+export function BoardContainer({ cards, title }: BoardContainerProps) {
+  return (
+    <div className="board-container">
+      <h3>{title}</h3>
+      <div className="board-cards">
+        {cards.map((card) => (
+          <Card
+            key={card.title}
+            content={card.content}
+            tags={card.tags}
+            title={card.title}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
